e7fb5da0ed7dc77206113dbdad248b566f5a35d0
Silent_Coder <36187018+Silentxknight@users.noreply.github.com>
2019-06-02 20:13:10 +0530

    Quick-Tests
    This Repo is for my quick tests

---
+++ main.js
@@ -0,0 +1,38 @@
+var bg="url(images/hover0.jpg)";
+var bgtime="all .2s";
+function hov(){
+    
+  document.body.style.background="url(images/home.jpg)"
+  document.body.style.backgroundSize="cover";
+ 
+  document.body.style.transition=bgtime;
+}
+function hovout(){
+   document.body.style.background=bg;
+   document.body.style.backgroundSize="cover";
+   document.body.style.transition=bgtime;
+}
+function hov1(){
+    
+    document.body.style.background="url(images/hover1.jpg)"
+    document.body.style.backgroundSize="cover";
+    document.body.style.transition=bgtime;
+  }
+  function hov2(){
+    
+    document.body.style.background="url(images/hover2.jpg)"
+    document.body.style.backgroundSize="cover";
+    document.body.style.transition=bgtime;
+  }
+  function hov3(){
+    
+    document.body.style.background="url(images/hover3.jpg)"
+    document.body.style.backgroundSize="cover";
+    document.body.style.transition=bgtime;
+  }
+  function hov4(){
+    
+    document.body.style.background="url(images/land.jpg)"
+    document.body.style.backgroundSize="cover";
+    document.body.style.transition=bgtime;
+  }
